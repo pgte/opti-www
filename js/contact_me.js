54d91071cbebb5297057448d7660bb8555d647d3
Pedro Teixeira <i@pgte.me>
2017-03-09 10:10:02 +0000

    contact form

--- js/contact_me.js
+++ js/contact_me.js
@@ -23,7 +23,7 @@ $(function() {
             }
 
             $.ajax({
-                url: "./mail/contact_me.php",
+                url: "/contact",
                 type: "POST",
                 data: {
                     name: name,
@@ -38,7 +38,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>A sua mensagem foi enviada e em breve entraremos em contacto consigo. Obrigado!</strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
