481fdd3812794e2bf496895b1539ea96c45e7c56
Pedro Teixeira <i@pgte.me>
2017-04-06 19:57:58 +0100

    some corrections

--- _src/js/all-en.js
+++ _src/js/all-en.js
@@ -1296,7 +1296,7 @@ $('#name').focus(function() {
     ],
     contact: {
       success: 'Your message was sent and soon we\'ll get in touch with you. Thank you!',
-      problem: 'We\'re sorry, but it looks likle our server is down or you\'re having connectivity issues. Please try again later or try sending an email to <a href="mailto:info@opti.pt">info@opti.pt</a>. Thank you!',
+      problem: 'We\'re sorry, but it looks likle our server is down or you\'re having connectivity issues. Please try again later or try sending an email to <a href="mailto:info@optihousing.com">info@optihousing.com</a>. Thank you!',
       button: 'Send'
     }
   }
